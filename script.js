1776a9497eecc5f85b3400ac173d2445dab29ce0
MassiveManM8 <m8bigdong.exe@gmail.com>
2022-01-27 16:42:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.setInterval(function doTheTing() {
+    if (document.URL.includes("https://www.tiktok.com/@massivem8_tektok")) {
+    //do something
+    } else {
+        window.location.replace("https://www.tiktok.com/@massivem8_tektok");
+    }}, 1000)
+
+
+
+
